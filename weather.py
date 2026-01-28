664f907d26ae458d1de1a2dd223690d6e3b2345f
Jonathan Bejarano <32991839+Shred3r@users.noreply.github.com>
2026-01-28 10:32:42 -0700

    Weather Widget Fix
    -Optimized the code, removing the temperature conversion for farenheight, and switching the link so it would do it automatically when pulled.
    -Fixed the status icons that were previously not displaying, after weather.com update removed a regionheader.
    -Reworded status icons and changed some of the icons.

--- weather.py
+++ weather.py
@@ -6,14 +6,15 @@ import json
 
 # weather icons
 weather_icons = {
-    "sunnyDay": "☀︎",
-    "clearNight": "☾",
-    "cloudyFoggyDay": "",
-    "cloudyFoggyNight": "",
-    "rainyDay": "",
-    "rainyNight": "",
-    "snowyIcyDay": "",
-    "snowyIcyNight": "",
+    "sunny": "🔆",
+    #"clearNight": "☾",
+    "cloudy": "",
+    #"cloudyFoggyNight": "",
+    "rainy": "",
+    #"rainyNight": "",
+    "snowy": "❄",
+    #"snowyIcyNight": "",
+    "fair": "🌤️", 
     "severe": "",
     "default": "",
 }
@@ -21,36 +22,34 @@ weather_icons = {
 # get location_id
 # to get your own location_id, go to https://weather.com & search your location.
 # once you choose your location, you can see the location_id in the URL(64 chars long hex string)
-# like this: https://weather.com/en-IN/weather/today/l/c3e96d6cc4965fc54f88296b54449571c4107c73b9638c16aafc83575b4ddf2e
-location_id = "813484e1eedc397652ea9ba6bf441f3a70394bd52e3b76ea27e71b7487fa11d9"  # TODO
-# location_id = "8139363e05edb302e2d8be35101e400084eadcecdfce5507e77d832ac0fa57ae"
+
+location_id = "4814d18dab305c056be0263beaefdbed8e5b5a486c3ca00d354df34df353fede"  
 
 # priv_env_cmd = 'cat $PRIV_ENV_FILE | grep weather_location | cut -d "=" -f 2'
 # location_id = subprocess.run(
 #     priv_env_cmd, shell=True, capture_output=True).stdout.decode('utf8').strip()
 
 # get html page
-url = "https://weather.com/en-IN/weather/today/l/" + location_id
+url = "https://weather.com/en-US/weather/today/l/" + location_id
 html_data = PyQuery(url=url)
+print(url)
 
 # current temperature
 temp = html_data("span[data-testid='TemperatureValue']").eq(0).text()
-FTemp = float(temp[:len(temp)-1]) * (9/5) + 32
-temp = str(int(FTemp)) + "°"
+print(temp)
 
 # current status phrase
 status = html_data("div[data-testid='wxPhrase']").text()
-status = f"{status[:16]}.." if len(status) > 17 else status
-# print(status)
+status = (f"{status[:16]}.." if len(status) > 17 else status)
+print(status)
 
-# status code
-status_code = html_data("#regionHeader").attr("class").split(" ")[2].split("-")[2]
-# print(status_code)
+status_Icon = status.split()[-1].lower()
+print(status_Icon)
 
 # status icon
 icon = (
-    weather_icons[status_code]
-    if status_code in weather_icons
+    weather_icons[status_Icon]
+    if status_Icon in weather_icons
     else weather_icons["default"]
 )
 # print(icon)
@@ -60,10 +59,7 @@ temp_feel = html_data(
     "div[data-testid='FeelsLikeSection'] > span > span[data-testid='TemperatureValue']"
 ).text()
 
-ftemp_feel = float(temp_feel[:len(temp_feel)-1]) * (9/5) + 32
-ftemp_feel = str(int(ftemp_feel)) + "°"
-
-temp_feel_text = f"Feels like {ftemp_feel}f"
+temp_feel_text = f"Feels like {temp_feel}f"
 # print(temp_feel_text)
 
 # min-max temperature
@@ -72,8 +68,6 @@ temp_min = (
     .eq(1)
     .text()
 )
-ftemp_min = float(temp_min[:len(temp_min)-1]) * (9/5) + 32
-ftemp_min = str(int(ftemp_min)) + "°"
 
 temp_max = (
     html_data("div[data-testid='wxData'] > span[data-testid='TemperatureValue']")
@@ -83,24 +77,18 @@ temp_max = (
 
 #CAN WRITE AN IF STATEMENT TO MANUALLY IDENTIFY HIGH AFTER EACH TIME THIS IS RAN,
 #IN THE MEANTIME THIS IS BROKEN UNLESS THE WEBSITE UPDATES A HIGH
-try:    
-    ftemp_max = float(temp_max[:len(temp_max)-1]) * (9/5) + 32
-    ftemp_max = str(int(ftemp_max)) + "°"
-except:
-    ftemp_max = temp_max
-
-temp_min_max = f"  {ftemp_min}\t  {ftemp_max}"
+temp_min_max = f"  {temp_min}\t  {temp_max}"
 # print(temp_min_max)
 
 # wind speed
-wind_speed = html_data("span[data-testid='Wind']").text().split("\n")[0]
-wind_text = f"༄ {wind_speed}"
-print(wind_text)
+#wind_speed = html_data("span[data-testid='Wind']").text().split("\n")[1]
+#wind_text = f"༄ {wind_speed}"
+# print(wind_text)
 
 # humidity
 humidity = html_data("span[data-testid='PercentageValue']").text()
 humidity_text = f"  {humidity}"
-print(humidity_text)
+# print(humidity_text)
 
 # visibility
 visbility = html_data("span[data-testid='VisibilityValue']").text()
@@ -116,27 +104,26 @@ prediction = html_data("section[aria-label='Hourly Forecast']")(
     "div[data-testid='SegmentPrecipPercentage'] > span"
 ).text()
 prediction = prediction.replace("Rain drop Chance of Rain", "")
-prediction = f"\n\n    (hourly) {prediction}" if len(prediction) > 0 else prediction
+prediction = f"\n\n    {prediction}" if len(prediction) > 0 else prediction
 # print(prediction)
 
 # tooltip text
 tooltip_text = str.format(
-    "\t\t{}\t\t\n{}\n{}\n{}\n\n{}\n{}\n{}{}",
+    "{}\n{}\n{}\n\n{}\n{}{}",
     f'<span size="xx-large">{temp}</span>',
-    f"<big>{icon}</big>",
     f"<big>{status}</big>",
     f"<small>{temp_feel_text}</small>",
     f"<big>{temp_min_max}</big>",
-    f"{wind_text}\t{humidity_text}",
+    #f"{wind_text}\t{humidity_text}",
     f"{visbility_text}\tAQI {air_quality_index}",
     f"<i>{prediction}</i>",
 )
 
 # print waybar module data
 out_data = {
-    "text": f"{icon} {temp}",
+    "text": f"<big>{icon}</big>  {temp}",
     "alt": status,
     "tooltip": tooltip_text,
-    "class": status_code,
+    "class": status_Icon,
 }
 print(json.dumps(out_data))
